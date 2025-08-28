1238001e59dbc2de9547cf073a01b44e60453765
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-08-28 22:30:24 +0200

    add dto base class for rabbitmq

---
+++ EmailService/src/EmailService.Worker/MessageBus/RabbitMqDtoBase.cs
@@ -0,0 +1,8 @@
+﻿namespace EmailService.Worker.MessageBus
+{
+    public abstract class RabbitMqDtoBase
+    {
+        public required Guid CorrelationId { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
