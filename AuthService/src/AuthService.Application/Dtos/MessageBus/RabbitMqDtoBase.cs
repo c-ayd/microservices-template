06bda553b6805286b07ec3ecaa2253941f3326c5
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-08-28 23:10:00 +0200

    add dto base class for rabbitmq

---
+++ AuthService/src/AuthService.Application/Dtos/MessageBus/RabbitMqDtoBase.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthService.Application.Dtos.MessageBus
+{
+    public abstract class RabbitMqDtoBase
+    {
+        public required Guid CorrelationId { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
