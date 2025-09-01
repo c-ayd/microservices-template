09ed64a66ca8661a12c06fb6d9e565954c7c730f
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-09-01 17:54:38 +0200

    add grpc metadata dto to carry headers between layers

---
+++ AdminService/src/AdminService.Application/Dtos/Grpc/Client/GrpcMetadataDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AdminService.Application.Dtos.Grpc.Client
+{
+    public class GrpcMetadataDto
+    {
+        public required Guid UserId { get; set; }
+        public required string JwtBearerToken { get; set; }
+        public required Guid CorrelationId { get; set; }
+    }
+}
