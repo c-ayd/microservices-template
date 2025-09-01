9291674cf5d8f1c53e53d53805d53e24456d2b95
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-09-01 17:55:04 +0200

    add grpc exception to catch translate errors returned by grpc server

---
+++ AdminService/src/AdminService.Application/Exceptions/GrpcException.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace AdminService.Application.Exceptions
+{
+    public class GrpcException : Exception
+    {
+        public int StatusCode { get; private set; }
+        public HttpStatusCode HttpStatusCode { get; private set; }
+        public Exception Exception { get; private set; }
+
+        public GrpcException(int statusCode, HttpStatusCode httpStatusCode, Exception exception, string? message = null)
+            : base(message)
+        {
+            StatusCode = statusCode;
+            HttpStatusCode = httpStatusCode;
+            Exception = exception;
+        }
+    }
+}
