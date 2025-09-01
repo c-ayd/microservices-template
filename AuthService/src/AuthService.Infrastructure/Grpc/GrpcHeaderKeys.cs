18e81bd855286df24ae90919b6b5e5582d28d6f9
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-09-01 17:09:43 +0200

    add logging interceptor for grpc server side

---
+++ AuthService/src/AuthService.Infrastructure/Grpc/GrpcHeaderKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthService.Infrastructure.Grpc
+{
+    public static class GrpcHeaderKeys
+    {
+        public const string Timestamp = "X-Client-Timestamp";
+        public const string UserId = "X-Client-UserId";
+        public const string Authorization = "Authorization";
+    }
+}
---
+++ AuthService/src/AuthService.Infrastructure/Grpc/Server/Interceptors/LoggingInterceptor.cs
@@ -0,0 +1,48 @@
+﻿using Cayd.AspNetCore.FlexLog;
+using Grpc.Core;
+using Grpc.Core.Interceptors;
+using System.Globalization;
+
+namespace AuthService.Infrastructure.Grpc.Server.Interceptors
+{
+    public class LoggingInterceptor : Interceptor
+    {
+        private readonly IFlexLogger<LoggingInterceptor> _flexLogger;
+
+        public LoggingInterceptor(IFlexLogger<LoggingInterceptor> flexLogger)
+        {
+            _flexLogger = flexLogger;
+        }
+
+        private void AddLogDetails(Metadata metadata)
+        {
+            _flexLogger.LogContext.Timestamp = DateTime.Parse(metadata.GetValue(GrpcHeaderKeys.Timestamp)!, null, DateTimeStyles.RoundtripKind);
+
+            var userId = metadata.GetValue(GrpcHeaderKeys.UserId);
+            if (userId != null)
+            {
+                _flexLogger.LogInformation($"The call is made by {metadata.GetValue(GrpcHeaderKeys.UserId)}");
+            }
+        }
+
+        public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(TRequest request, ServerCallContext context, UnaryServerMethod<TRequest, TResponse> continuation)
+        {
+            try
+            {
+                AddLogDetails(context.RequestHeaders);
+
+                return await continuation(request, context);
+            }
+            catch (Exception exception)
+            {
+                _flexLogger.LogError(exception.Message, exception);
+
+                throw;
+            }
+            finally
+            {
+                _flexLogger.LogContext.ResponseStatusCode = (int)context.Status.StatusCode;
+            }
+        }
+    }
+}
